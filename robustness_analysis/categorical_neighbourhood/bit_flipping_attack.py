5554e5d1bc6637c0296db9601beee0a6a60bde67
tanjascats <ta.sarcavic@gmail.com>
2019-11-25 02:06:34 +0100

    Bit-flipping attack

---
+++ robustness_analysis/categorical_neighbourhood/bit_flipping_attack.py
@@ -0,0 +1,69 @@
+# this is a script that runs the experiments of a bit-flipping attack on
+# the scheme for categorical data based on a neighbourhood search
+# approach
+import random
+from datetime import datetime
+import numpy as np
+
+from attacks.bit_flipping_attack import BitFlippingAttack
+from schemes.categorical_neighbourhood.categorical_neighbourhood import CategoricalNeighbourhood
+
+n_experiments = 20  # (20) number of times we attack the same fingerprinted file
+n_fp_experiments = 50  # (50) number of times we run fp insertion
+
+fractions = np.array([0.01, 0.05, 0.1, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50])
+results = []
+gamma = 20; xi = 1; fingerprint_bit_length = 16
+
+scheme = CategoricalNeighbourhood(gamma=gamma, xi=xi, fingerprint_bit_length=fingerprint_bit_length)
+attack = BitFlippingAttack()
+
+f = open("robustness_analysis/categorical_neighbourhood/log/bit_flipping_attack.txt", "a+")
+
+for size in fractions:
+    # for reproducibility
+    seed = 332
+    random.seed(seed)
+
+    correct, misdiagnosis = 0, 0
+    for i in range(n_fp_experiments):
+        # fingerprint the data
+        secret_key = random.randint(0, 1000)
+        fp_dataset = scheme.insertion(dataset_name='german_credit', buyer_id=1, secret_key=secret_key)
+
+        for j in range(n_experiments):
+            # perform the attack
+            release_data = attack.run(dataset=fp_dataset, fraction=size)
+            # try to extract the fingerprint
+            suspect = scheme.detection(dataset_name='german_credit', real_buyer_id=1, secret_key=secret_key,
+                                dataset=release_data)
+            if suspect == 1:
+                correct += 1
+            elif suspect != -1:
+                    misdiagnosis += 1
+
+    # write to log file
+    f.write(str(datetime.fromtimestamp(int(datetime.timestamp(datetime.now())))))
+    f.write("\nseed: " + str(seed))
+    f.write("\nData: german credit")
+    f.write("\n(fraction flipped, gamma, xi, length of a fingerprint): " + str((size, gamma, xi,
+                                                                            fingerprint_bit_length)))
+    f.write("\nCorrect: " + str(correct) + "/" + str(n_experiments*n_fp_experiments))
+    f.write("\nWrong: " + str(n_experiments*n_fp_experiments - correct) + "/" + str(n_experiments*n_fp_experiments)
+          + "\n\t- out of which misdiagnosed: " + str(misdiagnosis))
+    f.write("\n\n--------------------------------------------------------------\n\n")
+
+    results.append(correct)
+
+f.write("SUMMARY\n")
+f.write(str(datetime.fromtimestamp(int(datetime.timestamp(datetime.now())))))
+f.write("\n(gamma, xi, length of a fingerprint): " + str((gamma, xi, fingerprint_bit_length)))
+f.write("\nCorrect: " + str(results) + "\n\t/" + str(n_experiments * n_fp_experiments))
+f.write("\n\n--------------------------------------------------------------\n\n")
+f.close()
+
+print("SUMMARY\n")
+print(str(datetime.fromtimestamp(int(datetime.timestamp(datetime.now())))))
+print("\n(gamma, xi, length of a fingerprint): " + str((gamma, xi, fingerprint_bit_length)))
+print("\nCorrect: " + str(results) + "\n\t/" + str(n_experiments * n_fp_experiments))
+
--- test.py
+++ test.py
@@ -3,7 +3,8 @@ from schemes.ak_scheme.ak_scheme import AKScheme
 from schemes.block_scheme.block_scheme import BlockScheme
 from schemes.two_level_scheme.two_level_scheme import TwoLevelScheme
 from schemes.categorical_neighbourhood.categorical_neighbourhood import CategoricalNeighbourhood
-from attacks.horizontal_subset_attack import SubsetAttack
+from attacks.horizontal_subset_attack import HorizontalSubsetAttack
+from attacks.bit_flipping_attack import BitFlippingAttack
 from utils import *
 
 
@@ -140,18 +141,40 @@ class TestCategoricalNeighbourhood(unittest.TestCase):
 
 class TestAttacks(unittest.TestCase):
     def test_subset_attack(self):
-        attack = SubsetAttack()
+        attack = HorizontalSubsetAttack()
         dataset, primary_key = import_dataset("german_credit")
         frac = 0.95
         result = attack.run(dataset=dataset, fraction=frac)
         self.assertEqual(len(result), frac*len(dataset))
 
     def test_small_subset_attack(self):
-        attack = SubsetAttack()
+        attack = HorizontalSubsetAttack()
         dataset, primary_key = import_dataset("german_credit")
         frac = 0.1
         result = attack.run(dataset=dataset, fraction=frac)
         self.assertEqual(len(result), frac*len(dataset))
+
+    def test_zero_bit_flipping_attack(self):
+        attack = BitFlippingAttack()
+        dataset, primary_key = import_dataset("german_credit")
+        frac = 0
+        result = attack.run(dataset=dataset, fraction=frac)
+        test_column = dataset.columns.tolist()[1]
+        self.assertEqual(result[test_column][:].tolist(), result[test_column][:].tolist())
+
+    def test_bit_flipping_attack(self):
+        attack = BitFlippingAttack()
+        dataset, primary_key = import_dataset("german_credit")
+        frac = 0.5
+        result = attack.run(dataset=dataset, fraction=frac)
+        self.assertEqual(result["property"][568], 'A122')
+
+    def test_bit_flipping_attack_1(self):
+        attack = BitFlippingAttack()
+        dataset, primary_key = import_dataset("german_credit")
+        frac = 0.5
+        result = attack.run(dataset=dataset, fraction=frac)
+        self.assertNotEqual(result['property'][568], dataset['property'][568])
 
 
 if __name__ == '__main__':
