7b09f64d451d22bf462baad24a3defe1e07c2284
Tanja Šarčević <tsarcevic@sba-research.org>
2023-08-02 23:31:43 +0200

    Superset with deletion implemented; experiment script added

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,6 +1,8 @@
 from ._flipping_attack import FlippingAttack
 from ._subset_attack import HorizontalSubsetAttack, VerticalSubsetAttack
+from ._superset_attack import SupersetWithDeletion
 
 __all__ = ['FlippingAttack',
            'HorizontalSubsetAttack',
-           'VerticalSubsetAttack']
+           'VerticalSubsetAttack',
+           'SupersetWithDeletion']
