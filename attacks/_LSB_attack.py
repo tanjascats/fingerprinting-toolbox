51dc70194d2da8adace07649c6e50e610e989a7c
Tanja Šarčević <tsarcevic@sba-research.org>
2023-08-03 01:30:47 +0200

    Add rounding attack

--- attacks/_LSB_attack.py
+++ attacks/_LSB_attack.py
@@ -1,9 +1,10 @@
+import sys
+
 from attacks._base import Attack
 import time
 import random
 from scipy.stats import binom
 from datasets import Dataset
-
 
 
 class FlippingAttack(Attack):
@@ -15,7 +16,7 @@ class FlippingAttack(Attack):
     Runs the attack; gets a copy of 'dataset' with 'strength' altered items
     strength [0,1]
     """
-    def run(self, dataset, strength, random_state=None, keep_columns=None):
+    def run(self, dataset, strength, random_state=None, keep_columns=None, xi=1):
         start = time.time()
         # never alters the ID or the target (if provided)
         altered = dataset.copy()
@@ -43,7 +44,9 @@ class FlippingAttack(Attack):
                 # print('---categorical iteration: ' + str(timestamp_categorical-iteration_start))
             else:
                 # numerical
-                new_value = value ^ 1  # flipping the least significant bit
+                # - choose randomly a bit-position
+                bit_position = random.choice([i for i in range(xi)])  # 0,...,x-1
+                new_value = value ^ (2**bit_position)  # flipping the one random LSB
                 # timestamp_numerical = time.time()
                 # print('---numerical iteration: ' + str(timestamp_numerical-iteration_start))
             altered.at[row, column] = new_value
@@ -70,3 +73,53 @@ class FlippingAttack(Attack):
         fm = 1 - pow(1 - b, fp_len)
         return fm
 
+
+class RoundingAttack(Attack):
+    def __init__(self):
+        super().__init__()
+
+    """
+    Runs the attack; gets a copy of 'dataset' with 'strength' altered items
+        strength [0,1]
+        xi - rounding factor; how many LSBs are going to be rounded to 0
+        skip_categorical - if False, the rounding is applied to categorical values by replacing with attribute mode
+    """
+    def run(self, dataset, strength, random_state=None, keep_columns=None, xi=1, skip_categorical=True):
+        start = time.time()
+        # never alters the ID or the target (if provided)
+        altered = dataset.copy()
+        column_choice = dataset.columns
+        if 'Id' in dataset.columns:
+            column_choice = column_choice.drop(labels=["Id"])
+        if keep_columns is not None:
+            column_choice = column_choice.drop(labels=keep_columns)
+        attribute_domain = {column: list(set(dataset[column][:])) for column in column_choice
+                            if dataset[column].dtype == 'O'}
+        modes = dataset.mode().loc[0]
+        # print('no. iterations: ' + str(int(strength * (dataset.size - len(dataset)))))
+        for i in range(int(strength * (dataset.size - len(dataset)))):
+            # iteration_start = time.time()
+            random_state += (random_state+1)*(i+1)
+            random.seed(random_state)
+            row = random.choice(dataset.index)
+            column = random.choice(column_choice)
+            value = dataset[column][row]
+            if dataset[column].dtype == 'O' and not skip_categorical:
+                # categorical - replace with mode value
+                new_value = modes[column]
+                # timestamp_categorical = time.time()
+                # print('---categorical iteration: ' + str(timestamp_categorical-iteration_start))
+            elif dataset[column].dtype == 'O' and skip_categorical:
+                continue  # todo: this is still counting as a modification
+            else:
+                # numerical
+                mask = sys.maxsize - (2**xi-1)
+                new_value = value & mask  # flipping the one random LSB
+                # timestamp_numerical = time.time()
+                # print('---numerical iteration: ' + str(timestamp_numerical-iteration_start))
+            altered.at[row, column] = new_value
+
+        print("Flipping attack runtime on " + str(strength * 100) + "% of entries: " +
+              str(round(time.time() - start, 2)) + " sec.")
+
+        return altered
--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,9 +1,10 @@
-from ._flipping_attack import FlippingAttack
+from ._LSB_attack import FlippingAttack, RoundingAttack
 from ._subset_attack import HorizontalSubsetAttack, VerticalSubsetAttack
 from ._superset_attack import SupersetWithDeletion
 from ._combination_attack import DeletionSupersetFlipping
 
 __all__ = ['FlippingAttack',
+           'RoundingAttack',
            'HorizontalSubsetAttack',
            'VerticalSubsetAttack',
            'SupersetWithDeletion',
